1ac73848e0449157f84f9e8f4ad3c4d40a57891a
S.Mohamed Suhail <suhailm67@gmail.com>
2026-06-27 17:12:55 +0530

    Update app.js

--- Microservices/gateway-service/app.js
+++ Microservices/gateway-service/app.js
@@ -4,6 +4,10 @@ const app = express();
 app.use(express.json());
 const port = 3003;
 
+app.get('/', (req, res) => {
+    res.send('Gateway Service is running');
+});
+
 app.get('/health', (req, res) => {
   res.json({ status: 'Gateway Service is healthy' });
 });
